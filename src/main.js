c26771b98caeed5a9034251abc708c516f7f7676
Price Comstock <price.comstock@gmail.com>
2018-09-06 09:04:08 -0500

    added fontawesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./scss/pcstyles.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 Vue.config.productionTip = false;
 
