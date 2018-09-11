9f69faecb484b65c5730da129cebd39ba034452a
Price Comstock <price.comstock@gmail.com>
2018-09-10 22:33:42 -0500

    got footer working with environment variables

--- src/components/nav/Footer.vue
+++ src/components/nav/Footer.vue
@@ -4,42 +4,44 @@
             <div class="level">
                 <div class="level-left">
                     <div class="level-item has-text-grey">
-                        website © 2018
+                        {{ footerText }} 
                     </div>
                 </div>
                 <div class="level-right is-size-7">
                     <div class="level-item">
                         <a class="button is-small is-light has-text-grey" href="https://blog.pricecomstock.com">by Price Comstock</a>
                     </div>
-                    <div class="level-item field has-addons">
-                        <p class="control">
-                            <a href="https://twitter.com/price_comstock" class="button has-text-grey is-small is-light">
-                                <span class="icon is-medium">
-                                    <i class="fab fa-lg fa-twitter"></i>
-                                </span>
-                            </a>
-                        </p>
-                        <!-- <p class="control">
-                            <a href="https://pricecomstock.com" class="button has-text-grey is-small is-light">
-                                <span class="icon is-medium">
-                                    <i class="fa fa-lg fa-address-card"></i>
-                                </span>
-                            </a>
-                        </p> -->
-                        <!-- <p class="control">
-                            <a href="https://blog.pricecomstock.com" class="button has-text-grey is-small is-light">
-                                <span class="icon is-medium">
-                                    <i class="fa fa-lg fa-comment-dots"></i>
-                                </span>
-                            </a>
-                        </p> -->
-                        <!-- <p class="control">
-                            <a href="https://github.com/pricecomstock" class="button has-text-grey is-small is-light">
-                                <span class="icon is-medium">
-                                    <i class="fab fa-lg fa-github"></i>
-                                </span>
-                            </a>
-                        </p> -->
+                    <div class="level-item">
+                        <div class="field has-addons">
+                          <p class="control" v-if="displaySocialLinks.twitter">
+                              <a href="https://twitter.com/price_comstock" class="button has-text-grey is-small is-light">
+                                  <span class="icon is-medium">
+                                      <i class="fab fa-lg fa-twitter"></i>
+                                  </span>
+                              </a>
+                          </p>
+                          <p class="control" v-if="displaySocialLinks.resume">
+                              <a href="https://pricecomstock.com" class="button has-text-grey is-small is-light">
+                                  <span class="icon is-medium">
+                                      <i class="fa fa-lg fa-user"></i>
+                                  </span>
+                              </a>
+                          </p>
+                          <p class="control" v-if="displaySocialLinks.blog">
+                              <a href="https://blog.pricecomstock.com" class="button has-text-grey is-small is-light">
+                                  <span class="icon is-medium">
+                                      <i class="fa fa-lg fa-comment-dots"></i>
+                                  </span>
+                              </a>
+                          </p>
+                          <p class="control" v-if="displaySocialLinks.github">
+                              <a :href="githubUrl" class="button has-text-grey is-small is-light">
+                                  <span class="icon is-medium">
+                                      <i class="fab fa-lg fa-github"></i>
+                                  </span>
+                              </a>
+                          </p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -51,7 +53,27 @@
 export default {
   name: "Footer",
   data() {
-    return {};
+    return {
+      appName: process.env.VUE_APP_TITLE,
+      year: new Date().getFullYear(),
+      displaySocialLinks: {
+        twitter: true,
+        resume: true,
+        blog: false,
+        github: true
+      },
+      githubUrl: process.env.VUE_APP_GITHUB_URL,
+      version: process.env.VUE_APP_VERSION
+    };
+  },
+  computed: {
+    footerText() {
+      if (this.version) {
+        return this.appName + " v" + this.version;
+      } else {
+        return this.appName + " ©" + this.year;
+      }
+    }
   }
 };
 </script>
